c328e3143201421f13400d1dfdbda052bd3cec9d
firazputraa <mfirazpsa.2004@gmail.com>
2025-05-20 21:39:19 +0700

    update: SignIn, SignUp, SearchPage

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -6,17 +7,17 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-[#F2F9FF] text-sky-800 px-6 py-4 shadow-md ">
-      <div className="container mx-auto flex items-center justify-around ">
+    <nav className="sticky top-0 left-0 w-full z-50 bg-[#F2F9FF] text-sky-800 px-6 py-4 shadow-md ">
+      <div className="container mx-auto flex items-center justify-between ">
 
         {/* Logo */}
-        <div className="text-2xl font-bold">
+        <Link to="/" className="text-2xl font-bold">
             <h1 className="text-3xl font-bold text-sky-800"><span className='italic text-sky-500'>biru</span>Laut</h1>
-        </div>
+        </Link>
 
         {/* Hamburger (mobile only) */}
         <div className="lg:hidden">
-          <button onClick={toggleMenu} className="focus:outline-none">
+          <Link onClick={toggleMenu} className="focus:outline-none">
             <svg
               className="w-6 h-6"
               fill="none"
@@ -26,39 +27,32 @@ const Navbar = () => {
             >
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-          </button>
+          </Link>
         </div>
 
-        {/* Links (desktop) */}
-        {/* <ul className="hidden lg:flex space-x-6">
-          <li><a href="" className="relative inline-block text-blue-600 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[1.5px] after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full"></a></li>
-          <li><a href="" className="relative inline-block text-blue-600 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[1.5px] after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full">Detail</a></li>
-          <li><a href="" className="relative inline-block text-blue-600 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-0 after:h-[1.5px] after:bg-blue-600 after:transition-all after:duration-300 hover:after:w-full">Destination</a></li>
-        </ul> */}
 
         {/* Auth Buttons (desktop) */}
         <div className="hidden lg:flex space-x-4">
-          <button className="text-sky-200 bg-sky-800 px-4 py-1 rounded hover:bg-sky-200 hover:text-sky-800 transition">SignIn</button>
-          <button className="border border-white px-4 py-1 rounded hover:bg-white hover:text-blue-600 transition">SignUp</button>
+          <Link to="/signin" className="text-sky-200 bg-sky-800 px-4 py-1 rounded hover:bg-sky-200 hover:text-sky-800 transition">SignIn</Link>
+          <Link to="/signup" className="border border-[#00859D] px-4 py-1 rounded hover:bg-white hover:text-blue-600 transition">SignUp</Link>
         </div>
       </div>
 
       {/* Mobile Menu */}
       {isOpen && (
-        <div className="lg:hidden mt-4 space-y-2 text-center  sm:w-24 ">
-          {/* <a href="/" className="block hover:text-sky-500">Home</a>
-          <a href="/about" className="block hover:text-sky-500">Detail</a>
-          <a href="/destination" className="block hover:text-sky-500">Destination</a> */}
-          <button 
+        <div className="lg:hidden mt-4 space-y-2 text-center sm:w-24">
+          <Link 
+            to="/signin"
             className="block w-full text-sky-200 bg-sky-800 hover:text-sky-800 hover:bg-sky-200 px-4 py-2 rounded-xl"
           >
             SignIn
-          </button>
-          <button 
+          </Link>
+          <Link 
+            to="/signup"
             className="block w-full border border-sky-800 px-4 py-2 hover:bg-sky-800 hover:text-blue-200 transition rounded-xl"
           >
             SignUp
-          </button>
+          </Link>
         </div>
       )}
     </nav>
