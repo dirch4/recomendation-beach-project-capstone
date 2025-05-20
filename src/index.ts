8e3523d76b4467ce49914041fa01fdf76190652d
FYMajid <fathiyakanmajid81@gmail.com>
2025-05-20 23:09:59 +0700

    create feature login and register and create model reviews and beach

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import dontenv from "dotenv";
 import express from "express";
+import userController from "./app/user/controller";
 
 dontenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.route("/").get((req, res) => {
-  res.send("Hello world");
-});
+app.use(express.json());
+
+app.use("/user", userController);
 
 app.listen(PORT, () =>
   console.log(`Server running on port http://localhost:${PORT}`)
